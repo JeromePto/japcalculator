07764080e6071f4473fe6deb6c4045d2eb2f400a
Jerome <petot.jerome@gmail.com>
2020-10-27 11:11:03 +0100

    parrot final

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,15 +1,15 @@
 exports.handler = async function(event, context) {
   const requestBody = JSON.parse(event.body);
   const message = requestBody.message;
-  if (message === "") {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({message: "Pas de message"}),
-    };
-  } else {
+  if (message) {
     return {
       statusCode: 200,
       body: JSON.stringify({message: message}),
+    };
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({message: "Pas de message"}),
     };
   }
 };
